25caa485e098b8374df33ef21254939c90866f98
Thayana Resende <thayanarm@gmail.com>
2019-08-01 23:08:29 -0300

    Capítulo 3.12

---
+++ src/main/java/com/example/algamoney/api/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,33 @@
+
+package com.example.algamoney.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		Long codigo = recursoCriadoEvent.getCodigo();
+
+		adicionarHeaderLocation(response, codigo);
+	}
+	
+	//Adiciona o header location no cabeçalho da resposta da requisição
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri(); //Retorna a URI(Que estou entendendo também como URL) onde está o recurso cadastrado
+		
+		response.setHeader("Location", uri.toASCIIString());// Mostra no Cabeçalho da requisição a URL onde está o recurso cadastrado
+	}
+
+}
